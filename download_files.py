10dbf2e195960ba0dc5c3522bdb1e1ac2f29a4fe
Igor Lemes <igor.s.lemes@gmail.com>
2023-01-12 23:31:21 -0300

    Download Files in Python

---
+++ download_files.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+import sys
+
+# Download a list of files from a text file
+# Usage: download_files.py <file_list.txt>
+
+# Check if the file list exists
+if not os.path.isfile(sys.argv[1]):
+    print("File list does not exist")
+    sys.exit(1)
+
+# Download the files to dataset/images and unzip them
+with open(sys.argv[1]) as file_list:
+    for line in file_list:
+        subprocess.run(["wget", line.strip(), "-P", "dataset/images"])
+        subprocess.run(["unzip", "-q", "dataset/images/*.zip", "-d", "dataset/images"])
+
+# End of script
