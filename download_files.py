88c76f2f9b0bfe8f8d7fa41c0426104dafdba702
Igor Lemes <igor.s.lemes@gmail.com>
2023-01-12 23:37:24 -0300

    Download Files in Python

--- download_files.py
+++ download_files.py
@@ -1,19 +1,31 @@
 import os
 import subprocess
 import sys
-
+import argparse
 # Download a list of files from a text file
 # Usage: download_files.py <file_list.txt>
+# Args
+#   file_list.txt: A text file containing a list of files to download
 
-# Check if the file list exists
-if not os.path.isfile(sys.argv[2]):
-    print("File list does not exist")
-    sys.exit(1)
 
-# Download the files to dataset/images and unzip them
-with open(sys.argv[2]) as file_list:
-    for line in file_list:
-        subprocess.run(["wget", line.strip(), "-P", "dataset/images"])
-        subprocess.run(["unzip", "-q", "dataset/images/*.zip", "-d", "dataset/images"])
+def main():
+    args = argparse.ArgumentParser()
+    args.add_argument("--file", type=str, help="File list to download")
+    args = args.parse_args()
 
-# End of script
+
+    # Check if the file list exists
+    if not os.path.isfile(args.file):
+        print("File list does not exist")
+        sys.exit(1)
+
+    # Download the files to dataset/images and unzip them
+    with open(args.file) as file_list:
+        for line in file_list:
+            subprocess.run(["wget", line.strip(), "-P", "dataset/images"])
+            subprocess.run(["unzip", "-q", "dataset/images/*.zip", "-d", "dataset/images"])
+
+    # End of script
+
+if __name__ == "__main__":
+    main()
